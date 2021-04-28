a13498de8f7ddb26dc138646ef292b4105df7d41
Jocélia da Hora O. Barbosa <76666712+JoceliaHora@users.noreply.github.com>
2021-04-28 17:45:59 -0300

    terceiro commit

--- main.js
+++ main.js
@@ -9,28 +9,46 @@ window.alert("Foi um prazer ter você aqui!")}
 else if (continuar == "1"){
 let res1 = window.prompt("Qual o resultado da soma 100+30")
 if (res1== "130"){
-    document.getElementById("respostascertas").innerHtml = "Resposta 1"
+    document.getElementById("respostascertas").innerText = "Resposta 1"
 }
 else if (res1!= "130") {
-    document.getElementById("respostaserradas").document.write("Resposta 1")
+    document.getElementById("respostaserradas").innerText = "Resposta 1"
 }
 
 }
 let res2= window.prompt("Qual o resultado da soma 50+5")
 if (res2=="55"){
-    document.getElementById ("respostascertas").innerHtml = "Resposta 2".document.write("Resposta 2")
+    document.getElementById ("respostascertas").innerText = "Resposta 2"
 }
 else if (res2!="55") {
-    document.getElementById("respostaserradas").document.write("Resposta 2")
+    document.getElementById("respostaserradas").innerText = "Resposta 2"
 }
 
 let res3= window.prompt("Qual resultado da soma 5+5")
 if (res3=="10"){
-    document.getElementById ("respostascertas").document.write("Resposta 3")
+    document.getElementById ("respostascertas").innerText = "Resposta 3"
 }
 else if(res3!="10"){
-    document.getElementById ("respostaserradas").document.write("Resposta 3")
+    document.getElementById ("respostaserradas").innerText = "Resposta 3"
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
