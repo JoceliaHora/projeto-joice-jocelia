befb5a30b0d2971ce7903fbbd579cf1260c7c423
Jocélia da Hora O. Barbosa <76666712+JoceliaHora@users.noreply.github.com>
2021-04-28 17:27:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,124 @@ let continuar =  window.prompt("Deseja continuar o quis?\n 1.Sim \n 2. Não")
 
 
 if (continuar == "2"){
-window.prompt("Foi um prazer ter você aqui!")}
+window.alert("Foi um prazer ter você aqui!")}
+else if (continuar == "1"){
+let res1 = window.prompt("Qual o resultado da soma 100+30")
+if (res1== "130"){
+    document.getElementById("respostascertas").innerHtml = "Resposta 1"
+}
+else if (res1!= "130") {
+    document.getElementById("respostaserradas").document.write("Resposta 1")
+}
+
+}
+let res2= window.prompt("Qual o resultado da soma 50+5")
+if (res2=="55"){
+    document.getElementById ("respostascertas").innerHtml = "Resposta 2".document.write("Resposta 2")
+}
+else if (res2!="55") {
+    document.getElementById("respostaserradas").document.write("Resposta 2")
+}
+
+let res3= window.prompt("Qual resultado da soma 5+5")
+if (res3=="10"){
+    document.getElementById ("respostascertas").document.write("Resposta 3")
+}
+else if(res3!="10"){
+    document.getElementById ("respostaserradas").document.write("Resposta 3")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
