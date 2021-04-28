32e62fd0bd9cc08fecc86a304f61673b45d7e7fc
Jocélia da Hora O. Barbosa <76666712+JoceliaHora@users.noreply.github.com>
2021-04-28 15:34:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let nome = window.prompt("Qual seu nome?") 
+window.alert(`É um prazer te conhecer, ${nome}`)
+
+let continuar =  window.prompt("Deseja continuar o quis?\n 1.Sim \n 2. Não")
+
+
+if (continuar == "2"){
+window.prompt("Foi um prazer ter você aqui!")}
+
+
+
+
+
+
